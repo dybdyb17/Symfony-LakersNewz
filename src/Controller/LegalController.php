8ba2ecc6814ee1ed12e47cd44f8bd58913f86917
Boudiaf Dybril <dybrilboudiaf14@gmail.com>
2026-07-15 10:42:15 +0200

    page légal faites

---
+++ src/Controller/LegalController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LegalController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'app_mentions_legales', methods: ['GET'])]
+    public function mentionsLegales(): Response
+    {
+        return $this->render('legal/mentions_legales.html.twig');
+    }
+}
